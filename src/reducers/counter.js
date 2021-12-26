10ba523992e64191ff75565aab0234c796877cc0
Rahul <rahulrulz680@gmail.com>
2021-12-26 19:03:59 +0530

    refactored

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,14 +1,14 @@
 import { produce } from 'immer';
 
-const counterReducer = (state = { value: 0 }, action) => produce(state, draft  => {
+const counterReducer = produce((draft = { value: 0 }, action)  => {
     switch (action.type) {
         case 'INCREMENT':
             draft.value += 1;
-            break;
+            return draft;
 
         case 'DECREMENT':
             draft.value -= 1;
-            break;
+            return draft;
     
         default:
             return draft;
